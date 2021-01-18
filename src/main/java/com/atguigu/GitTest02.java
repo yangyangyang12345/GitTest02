760d3d103347b89fd31d673f8470a711b53e2c99
杨海东 <49123635@qq.com>
2021-01-18 17:07:35 +0800

    添加4,合并冲突

--- src/main/java/com/atguigu/GitTest02.java
+++ src/main/java/com/atguigu/GitTest02.java
@@ -4,5 +4,6 @@ public class GitTest02 {
     public static void main(String[] args) {
         System.out.println("1111111111111");
         System.out.println("2222222222222");
+        System.out.println("4444444444444");
     }
 }
