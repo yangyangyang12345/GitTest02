bd2559afd70dc7628e901bb5d2b2815df55f0103
yangyangyang12345 <77606676+yangyangyang12345@users.noreply.github.com>
2021-01-18 19:01:55 +0800

    Update GitTest02.java

--- src/main/java/com/atguigu/GitTest02.java
+++ src/main/java/com/atguigu/GitTest02.java
@@ -7,5 +7,6 @@ public class GitTest02 {
         System.out.println("3333333333333");
         System.out.println("4444444444444");
         System.out.println("bbbbbbbbbbbbb"); 
+        System.out.println("xxxxxxxxxxxxxxx"); 
     }
 }
